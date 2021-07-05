058a8c1c9239244650571b05cf8836f4c25b678b
DIEGO VILLANUEVA FERREYRA <19121092@morelia.tecnm.mx>
2021-07-05 11:37:34 -0500

    Prueba ver todos

--- TAPU3_PROYECTO/Form1.cs
+++ TAPU3_PROYECTO/Form1.cs
@@ -14,9 +14,9 @@ namespace TAPU3_PROYECTO
 {
     public partial class Form1 : Form
     {
-        private String Diego = "http://192.168.1.70:80/my_sge/acceso.php";
+        private String Diego = "http://192.168.1.70:80/my_sge/verTodos.php";
         private String Marco = "http://192.168.0.10:443/WS2021/my_sge/acceso.php";
-        private String myWs = "Marco";
+        private String myWs = "Diego";
         private String apiClima = "https://api.openweathermap.org/data/2.5/weather?q=Morelia&lang=es&units=metric&appid=d7830285419c508e732ba5357594d148";
         private String usr;
         private String pass;
@@ -38,7 +38,7 @@ namespace TAPU3_PROYECTO
 
             HttpClient client = new HttpClient();
             //mandando parametros para accesar a la bd con ws
-            String content = await client.GetStringAsync(Marco+"/?usr="+usr+"&pass="+pass);
+            String content = await client.GetStringAsync(Diego/*+"/?usr="+usr+"&pass="+pass*/);
 
             Console.WriteLine(content);
 
@@ -52,8 +52,6 @@ namespace TAPU3_PROYECTO
             catch (Exception ex)
             {
                 Console.WriteLine("Error "+ex);
-
-                throw;
             }
 
           
