46a605d8ce68d0b1c1760382ed6bd613d1bce911
DIEGO VILLANUEVA FERREYRA <19121092@morelia.tecnm.mx>
2021-07-04 21:14:44 -0500

    Formularios Modificados

---
+++ TAPU3_PROYECTO/Datos.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace TAPU3_PROYECTO
+{
+    partial class Datos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCambiar = new System.Windows.Forms.Button();
+            this.textTel = new System.Windows.Forms.TextBox();
+            this.textNueva = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textSemestre = new System.Windows.Forms.TextBox();
+            this.textCarrera = new System.Windows.Forms.TextBox();
+            this.textNoControl = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnCambiar);
+            this.groupBox1.Controls.Add(this.textTel);
+            this.groupBox1.Controls.Add(this.textNueva);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.groupBox1.Location = new System.Drawing.Point(86, 330);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(504, 313);
+            this.groupBox1.TabIndex = 29;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos Modificables:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(20, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Telefono:";
+            // 
+            // btnCambiar
+            // 
+            this.btnCambiar.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnCambiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCambiar.ForeColor = System.Drawing.Color.White;
+            this.btnCambiar.Location = new System.Drawing.Point(200, 165);
+            this.btnCambiar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCambiar.Name = "btnCambiar";
+            this.btnCambiar.Size = new System.Drawing.Size(292, 33);
+            this.btnCambiar.TabIndex = 19;
+            this.btnCambiar.Text = "Modificar datos";
+            this.btnCambiar.UseVisualStyleBackColor = false;
+            // 
+            // textTel
+            // 
+            this.textTel.AllowDrop = true;
+            this.textTel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textTel.Location = new System.Drawing.Point(151, 43);
+            this.textTel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textTel.Name = "textTel";
+            this.textTel.Size = new System.Drawing.Size(340, 27);
+            this.textTel.TabIndex = 13;
+            // 
+            // textNueva
+            // 
+            this.textNueva.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNueva.Location = new System.Drawing.Point(151, 105);
+            this.textNueva.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textNueva.Name = "textNueva";
+            this.textNueva.Size = new System.Drawing.Size(340, 27);
+            this.textNueva.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(11, 105);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 24);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Contraseña:";
+            // 
+            // textSemestre
+            // 
+            this.textSemestre.Enabled = false;
+            this.textSemestre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSemestre.Location = new System.Drawing.Point(224, 258);
+            this.textSemestre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textSemestre.Name = "textSemestre";
+            this.textSemestre.ReadOnly = true;
+            this.textSemestre.Size = new System.Drawing.Size(340, 27);
+            this.textSemestre.TabIndex = 28;
+            // 
+            // textCarrera
+            // 
+            this.textCarrera.Enabled = false;
+            this.textCarrera.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textCarrera.Location = new System.Drawing.Point(224, 201);
+            this.textCarrera.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textCarrera.Name = "textCarrera";
+            this.textCarrera.ReadOnly = true;
+            this.textCarrera.Size = new System.Drawing.Size(340, 27);
+            this.textCarrera.TabIndex = 27;
+            // 
+            // textNoControl
+            // 
+            this.textNoControl.Enabled = false;
+            this.textNoControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNoControl.Location = new System.Drawing.Point(224, 145);
+            this.textNoControl.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textNoControl.Name = "textNoControl";
+            this.textNoControl.ReadOnly = true;
+            this.textNoControl.Size = new System.Drawing.Size(340, 27);
+            this.textNoControl.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(82, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 24);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Carrera:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(81, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 24);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Semestre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(81, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 24);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "No. de Control:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(82, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 24);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Nombre:";
+            // 
+            // textNombre
+            // 
+            this.textNombre.Enabled = false;
+            this.textNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNombre.Location = new System.Drawing.Point(224, 94);
+            this.textNombre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.ReadOnly = true;
+            this.textNombre.Size = new System.Drawing.Size(340, 27);
+            this.textNombre.TabIndex = 21;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(218, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(265, 36);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "Datos Personales";
+            // 
+            // Datos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.ClientSize = new System.Drawing.Size(688, 686);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textSemestre);
+            this.Controls.Add(this.textCarrera);
+            this.Controls.Add(this.textNoControl);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textNombre);
+            this.Name = "Datos";
+            this.Text = "Datos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCambiar;
+        private System.Windows.Forms.TextBox textTel;
+        private System.Windows.Forms.TextBox textNueva;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textSemestre;
+        private System.Windows.Forms.TextBox textCarrera;
+        private System.Windows.Forms.TextBox textNoControl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ TAPU3_PROYECTO/Examenes.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace TAPU3_PROYECTO
+{
+    partial class Examenes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(231, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 36);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "Examen";
+            // 
+            // Examenes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.ClientSize = new System.Drawing.Size(648, 631);
+            this.Controls.Add(this.label6);
+            this.Name = "Examenes";
+            this.Text = "Examenes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+    }
+}
