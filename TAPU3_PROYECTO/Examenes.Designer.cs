d58aea751538077dbf45659ca59155320ad08de7
DIEGO VILLANUEVA FERREYRA <19121092@morelia.tecnm.mx>
2021-07-07 22:46:30 -0500

    Preguntas de Examen Modificación

--- TAPU3_PROYECTO/Examenes.Designer.cs
+++ TAPU3_PROYECTO/Examenes.Designer.cs
@@ -29,29 +29,71 @@ namespace TAPU3_PROYECTO
         /// </summary>
         private void InitializeComponent()
         {
-            this.label6 = new System.Windows.Forms.Label();
+            this.labelMateria = new System.Windows.Forms.Label();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label6
+            // labelMateria
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label6.Location = new System.Drawing.Point(231, 35);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(128, 36);
-            this.label6.TabIndex = 31;
-            this.label6.Text = "Examen";
+            this.labelMateria.AutoSize = true;
+            this.labelMateria.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMateria.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelMateria.Location = new System.Drawing.Point(748, 31);
+            this.labelMateria.Name = "labelMateria";
+            this.labelMateria.Size = new System.Drawing.Size(128, 36);
+            this.labelMateria.TabIndex = 31;
+            this.labelMateria.Text = "Examen";
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSiguiente.Location = new System.Drawing.Point(726, 338);
+            this.btnSiguiente.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(220, 49);
+            this.btnSiguiente.TabIndex = 34;
+            this.btnSiguiente.Text = "Siguiente";
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("MS Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(32, 213);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(1479, 34);
+            this.comboBox1.TabIndex = 33;
+            this.comboBox1.Text = "Respuestas...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(26, 103);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 36);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "1.- Pregunta: ";
             // 
             // Examenes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumSeaGreen;
-            this.ClientSize = new System.Drawing.Size(648, 631);
-            this.Controls.Add(this.label6);
+            this.ClientSize = new System.Drawing.Size(1550, 481);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelMateria);
             this.Name = "Examenes";
             this.Text = "Examenes";
+            this.Load += new System.EventHandler(this.Examenes_LoadAsync);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,6 +101,9 @@ namespace TAPU3_PROYECTO
 
         #endregion
 
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelMateria;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
