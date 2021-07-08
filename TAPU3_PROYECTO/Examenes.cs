876f4938a2d0bfd9a8345365c414a53f73ff084a
MARCO ANTONIO MERCADO <19121052@morelia.tecnm.mx>
2021-07-08 13:43:55 -0500

    Examenes calificados automaticamente y mostrar respuestas (semestre) y solo poo en sem2

--- TAPU3_PROYECTO/Examenes.cs
+++ TAPU3_PROYECTO/Examenes.cs
@@ -16,11 +16,12 @@ namespace TAPU3_PROYECTO
     {
         int semestre = FormMenu.semestre;
         String materia = FormMenu.materiaExamen;
+        private int id_materia=0;
 
         //guardar semestre, respuesta y carrera
         private String car, inciso;
         private int cont = 0, numRespuestas = 0, idP=0, cont2 = 0;
-        private bool respuestaCorrecta = false;
+        private int respuestaCorrecta;
 
         private String materiaCalif;
 
@@ -36,8 +37,13 @@ namespace TAPU3_PROYECTO
         private static String Diegor = "http://192.168.1.70/my_sge/pyr.php";
         private static String Marcor = "http://192.168.1.10/my_sge/pyr.php";
 
+        //para calificar
+        private static String DiegoC = "http://192.168.1.70/my_sge/calificar.php";
+        private static String MarcoC = "http://192.168.1.10/my_sge/calificar.php";
+
         String ws = Marco;
         String wsr = Marcor;
+        String wsc = MarcoC;
         public Examenes()
         {
             InitializeComponent();
@@ -59,6 +65,8 @@ namespace TAPU3_PROYECTO
 
                 if (jIndex.ContainsKey("pregunta"))
                 {
+                    id_materia = (int)jIndex.GetValue("id_materia");
+                    MessageBox.Show("id_materia = "+id_materia);
                     comboBox1.Items.Clear();
                     label1.Text += (String)jIndex.GetValue("pregunta");
                     idP = (int)jIndex.GetValue("id");
@@ -76,33 +84,36 @@ namespace TAPU3_PROYECTO
             //MessageBox.Show(idP.ToString());
             //cargar respuestas 
             HttpClient client2 = new HttpClient();
-            String respuestas = await client2.GetStringAsync(wsr + "?materia=" + materia
-                + "&semestre=" + semestre + "&idPregunta=" + idP);
+            String respuestas = await client2.GetStringAsync(wsr);
             try
             {
                 JObject jsonObject2 = JObject.Parse(respuestas);
                 jOutput2 = (JArray)jsonObject2.GetValue("output");
-                Console.WriteLine(jOutput2.ToString());
-                jIndex2 = (JObject)jOutput2[cont2];
 
-                if (jIndex2.ContainsKey("respuesta"))
+                //MessageBox.Show(idP.ToString());
+
+                for (int i = 0; i < jOutput2.Count; i++)
                 {
-                    //comboBox1.Items.Clear();
-
-                    MessageBox.Show(jIndex2.GetValue("correcta").ToString());
-                    //respuestaCorrecta = (bool)jIndex2.GetValue("correcta");
-
-                    if ((int)jIndex2.GetValue("id_pregunta") == idP) 
+                    JObject jRes = (JObject)jOutput2[i];
+                   
+                    if ((int)jRes.GetValue("id_pregunta") == idP)
                     {
-                        for (int i = 0; i < jOutput2.Count; i++)
+                    
+                        comboBox1.Items.Add(jRes.GetValue("respuesta"));
+                        /*JObject re = (JObject)comboBox1.SelectedItem;
+                        if ((int)re.GetValue("correcta") == 1)
                         {
-                            JObject jRes = (JObject)jOutput2[i];
-                            comboBox1.Items.Add(jRes.GetValue("respuesta"));
+                            numRespuestas += 1;
+                            MessageBox.Show("Correcto!");
                         }
-                        
+                        else 
+                        {
+                            numRespuestas += 0;
+                            MessageBox.Show("Andas en las nubes!");
+                        }*/
+
                     }
                 }
-                //labelNombre.Text += ": " + nombre1;
 
             }
             catch (Exception ex)
@@ -116,8 +127,9 @@ namespace TAPU3_PROYECTO
         private void btnSiguiente_Click(object sender, EventArgs e)
         {
             cont++;
+            revisar();
             pregunta();
-            /*if (!comboBox1.SelectedIndex.ToString().Equals("-1"))
+            if (!comboBox1.SelectedIndex.ToString().Equals("-1"))
             {
                 if (comboBox1.SelectedItem.Equals(respuestaCorrecta))
                 {
@@ -129,12 +141,11 @@ namespace TAPU3_PROYECTO
             else
             {
                 MessageBox.Show("Selecciona una respuesta");
-            }*/
+            }
         }
 
-        private void pregunta()
+        private async void pregunta()
         {
-            //Sea
             if (cont < 10)
             {
                 label1.Text = "";
@@ -147,14 +158,96 @@ namespace TAPU3_PROYECTO
                     label1.Text = (cont+1) + ".-Pregunta ";
                     label1.Text += (String)jIndex.GetValue("pregunta");
                     idP = (int)jIndex.GetValue("id");
+
+                    //agregar respuestas 
+                    try
+                    {
+                        HttpClient client2 = new HttpClient();
+                        String respuestas = await client2.GetStringAsync(wsr);
+                        JObject jsonObject2 = JObject.Parse(respuestas);
+                        jOutput2 = (JArray)jsonObject2.GetValue("output");
+
+                        //MessageBox.Show(idP.ToString());
+                        //respuestaCorrecta = (bool)jIndex2.GetValue("correcta");
+
+                        for (int i = 0; i < jOutput2.Count; i++)
+                        {
+                            JObject jRes = (JObject)jOutput2[i];
+
+                            if ((int)jRes.GetValue("id_pregunta") == idP)
+                            {
+                                comboBox1.Items.Add(jRes.GetValue("respuesta"));
+                            }
+                        }
+
+                    }
+                    catch (Exception ix)
+                    {
+                        Console.WriteLine("Error: "+ix);
+                        MessageBox.Show("Error al accesar a la BD");
+                        throw;
+                    }
                 }
             }
             else
             {
                 MessageBox.Show("Usted obtuvo " + numRespuestas);
-                //calificarExamen(numRespuestas);
+                calificarExamen();
                 this.Dispose();
             }
         }
+
+        public void revisar() 
+        {
+            for (int i = 0; i < jOutput2.Count; i++)
+            {
+
+                JObject re = (JObject)jOutput2[i];
+
+                //revisar respuestas que coincida con el item seleccionado
+                if (re.GetValue("respuesta").ToString() == comboBox1.SelectedItem.ToString())
+                {
+                    //MessageBox.Show(re.GetValue("respuesta").ToString());
+                    //revisar valor 
+                    if ((int)re.GetValue("correcta") == 1)
+                    {
+                        numRespuestas += 10;
+                        //MessageBox.Show("Correcto!");
+                    }
+                    else
+                    {
+                        numRespuestas += 0;
+                        //MessageBox.Show("Andas en las nubes!");
+                    }
+
+                }
+
+               
+            }
+           
+        }
+
+        public async void calificarExamen()
+        {
+            try
+            {
+                MessageBox.Show("user: "+Form1.index +" materia: "+id_materia+" califiacion:"+numRespuestas);
+                HttpClient client3 = new HttpClient();
+                String content = await client3.GetStringAsync(wsc + "?usr=" + Form1.index
+                    + "&materia=" + id_materia + "&calif=" + numRespuestas);
+                MessageBox.Show(content+" Calificado");
+
+            }
+            catch (Exception ox)
+            {
+                Console.WriteLine("Error "+ox);
+                MessageBox.Show("Error, intente mas tarde...");
+                throw;
+            }
+         
+
+        }
+
+
     }
 }
